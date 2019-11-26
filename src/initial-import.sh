427003c441b0c77a1bb77ee1cc49678c8ddd7f28
Steffen Volkmann <volkmann@devpool.net>
2019-11-26 09:27:48 +0100

    update path of mapnik style and lua script

--- src/initial-import.sh
+++ src/initial-import.sh
@@ -2,10 +2,9 @@
 
 BASE=/storage_ssd/osm_replicate
 FLATNODEFILE=$BASE/flatnode.dat
-STYLE=/etc/mapnik-osm-data/openstreetmap-carto-de/hstore-only.style
-LUA=/etc/mapnik-osm-data/openstreetmap-carto-de/openstreetmap-carto.lua
+STYLE=/replication/src/openstreetmap-carto/openstreetmap-carto.style
+LUA=/replication/src/openstreetmap-carto/openstreetmap-carto.lua
 
 export PGPASSWORD=osm
 
 osm2pgsql -G -d osm -U osm -s -C16000 -S $STYLE --flat-nodes $FLATNODEFILE --hstore --tag-transform-script $LUA -p planet_osm_hstore $1
-
