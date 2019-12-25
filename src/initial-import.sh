0cc72d8e23883c593de150958912509bcc523c67
Steffen Volkmann <volkmann@devpool.net>
2019-12-25 15:54:23 +0100

    initial-import: modification for usage in docker container

--- src/initial-import.sh
+++ src/initial-import.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+set -x
+
 BASE=/storage_ssd/osm_replicate
-FLATNODEFILE=$BASE/flatnode.dat
+FLATNODEFILE=/nodes/flat_nodes.bin
 STYLE=/replication/src/openstreetmap-carto/openstreetmap-carto.style
 LUA=/replication/src/openstreetmap-carto/openstreetmap-carto.lua
 
-export PGPASSWORD=osm
+export PGPASSWORD=renderer
+PG_USER=renderer
+PG_DBNAME=gis
+PG_HOST=osmpsql
 
-osm2pgsql -G -d osm -U osm -s -C16000 -S $STYLE --flat-nodes $FLATNODEFILE --hstore --tag-transform-script $LUA -p planet_osm_hstore $1
+echo $date
+osm2pgsql -U $PG_USER -H $PG_HOST -d $PG_DBNAME -G -s -C16000 -S $STYLE --flat-nodes $FLATNODEFILE --hstore --tag-transform-script $LUA -p planet_osm_hstore $1
+echo $date
