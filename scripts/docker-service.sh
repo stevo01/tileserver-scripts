c442144c8b1300a524bfed0e6f14412055e71104
Steffen Volkmann <volkmann@devpool.net>
2019-12-25 15:53:09 +0100

    docker-service: integrate start and stop script

--- scripts/docker-service.sh
+++ scripts/docker-service.sh
@@ -6,9 +6,47 @@ set -x
 
 IMAGE_NAME="osm-db-utils"
 
+function start() {
+docker run \
+		--name $IMAGE_NAME \
+		--rm=false \
+		-e OSM2PGSQL_EXTRA_ARGS="--flat-nodes /nodes/flat_nodes.bin -C 4096" \
+		--restart unless-stopped \
+		--detach \
+		--memory=32G \
+		--hostname osmdbutils \
+		--link osm-tileserver-db:osm-tileserver-db \
+		-v openstreetmap-flat:/nodes \
+		-v $PWD/volumes/transfer:/transfer \
+		-v $PWD/volumes/work:/replication/work \
+		-v $PWD/volumes/download:/replication/download \
+		$IMAGE_NAME \
+		run
+}
+
+function stop() {
+  docker container stop $IMAGE_NAME
+  docker container rm $IMAGE_NAME
+}
+
+function build() {
+  docker build -t $IMAGE_NAME ./src
+}
+
 case "$1" in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  restart)
+    build
+    stop
+    start
+    ;;
   build)
-		docker build -t $IMAGE_NAME ./src
+    build
 	;;
   connect)
 		docker exec -i -t $IMAGE_NAME /bin/bash
--- scripts/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-IMAGE_NAME="osm-db-utils"
-
-docker container stop $IMAGE_NAME
-docker container rm $IMAGE_NAME
