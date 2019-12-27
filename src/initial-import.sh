5e815f2b101d13d93f56e30e09f258993e5716b9
Steffen Volkmann <volkmann@devpool.net>
2019-12-27 09:28:08 +0100

    import: remove osm2psql option "-p planet_osm_hstore"

--- src/initial-import.sh
+++ src/initial-import.sh
@@ -16,7 +16,7 @@ PG_HOST=osmpsql
 echo "initial import osm database" 2>&1 | tee $LOGFILE
 
 echo "start import $(date)" 2>&1 | tee -a $LOGFILE
-osm2pgsql -U $PG_USER -H $PG_HOST -d $PG_DBNAME --create -G -s --number-processes 4 -C16000 -S $STYLE --flat-nodes $FLATNODEFILE --hstore --tag-transform-script $LUA -p planet_osm_hstore $1 2>&1 | tee -a $LOGFILE
+osm2pgsql -U $PG_USER -H $PG_HOST -d $PG_DBNAME --create -G -s --number-processes 4 -C16000 -S $STYLE --flat-nodes $FLATNODEFILE --hstore --tag-transform-script $LUA $1 2>&1 | tee -a $LOGFILE
 echo "end import $(date)" 2>&1 | tee -a $LOGFILE
 
 # log database settings
