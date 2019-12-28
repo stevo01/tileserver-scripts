b1860060d80b0d8c3846796ac3786f389e7d084f
Steffen Volkmann <volkmann@devpool.net>
2019-12-28 06:04:22 +0100

    update logfile generation

--- src/initial-import.sh
+++ src/initial-import.sh
@@ -13,11 +13,15 @@ PG_USER=renderer
 PG_DBNAME=gis
 PG_HOST=osmpsql
 
-echo "initial import osm database" 2>&1 | tee $LOGFILE
 
-echo "start import $(date)" 2>&1 | tee -a $LOGFILE
+function log_info {
+    today=`date +%Y-%m-%d.%H:%M:%S`
+    echo "[INFO ] $today $1" 2>&1 | tee -a $LOGFILE
+}
+
+loginfo "start initial import of database $(date)"
 osm2pgsql -U $PG_USER -H $PG_HOST -d $PG_DBNAME --create -G -s --number-processes 4 -C16000 -S $STYLE --flat-nodes $FLATNODEFILE --hstore --tag-transform-script $LUA $1 2>&1 | tee -a $LOGFILE
-echo "end import $(date)" 2>&1 | tee -a $LOGFILE
+loginfo "end import $(date)" 2>&1 | tee -a $LOGFILE
 
 # log database settings
 psql -U $PG_USER -h $PG_HOST -d $PG_DBNAME -c "select name, setting from pg_settings" 2>&1 | tee -a $LOGFILE
--- src/replicate-loop.sh
+++ src/replicate-loop.sh
@@ -9,20 +9,29 @@ SEQFILE=$BASE/data/sequence_file
 STYLE=/replication/src/openstreetmap-carto/openstreetmap-carto.style
 LUA=/replication/src/openstreetmap-carto/openstreetmap-carto.lua
 MINZOOM=13
+LOGFILE=/replication/work/replication.log
+
 export PGPASSWORD=renderer
 PG_USER=renderer
 PG_DBNAME=gis
 PG_HOST=osmpsql
 
-LOGFILE=/replication/work/replication.log
+function log_info {
+    today=`date +%Y-%m-%d.%H:%M:%S`
+    echo "[INFO ] $today $1" 2>&1 | tee -a $LOGFILE
+}
 
-STARTIME=$(date)
+function log_error {
+    today=`date +%Y-%m-%d.%H:%M:%S`
+    echo "[ERROR] $today $1" 2>&1 | tee -a $LOGFILE
+    exit 1
+}
+
 
 # check if sequence file exists
 if ! [ -f $SEQFILE ]
 then
-  >&2 echo "sequence file $SEQFILE does not exist"
-  exit 1
+  log_error "error: sequence file $SEQFILE does not exist"
 fi
 
 LOCKFILE=$BASE/data/replicate.lock
@@ -37,13 +46,12 @@ then
    if kill -0 $OTHERPID 2>/dev/null
    then
       # exit script if another instance if script is running
-      echo "other process ($OTHERPID) active"
-      exit 1
+      log_error "other process ($OTHERPID) active"
    fi
 fi
 
 # create lockfile
-echo $$ > $LOCKFILE
+log_info "bgn replication $(date)"
 
 # calculate and show lag (in minutes)
 START=`date +%s`
@@ -51,8 +59,7 @@ eval $(curl -s https://planet.osm.org/replication/minute/state.txt |grep sequenc
 local_sequence=$(cat $SEQFILE)
 LAG=$(expr $sequenceNumber - $local_sequence)
 
-echo "start update: osmosis replication is ${LAG} minutes behind upstream"
-echo "start update: osmosis replication is ${LAG} minutes behind upstream" >> $LOGFILE
+log_info "start update: osmosis replication is ${LAG} minutes behind upstream"
 
 THISFILE=$BASE/data/replicate-$START.osc
 MERGEDFILE=$BASE/data/merged.osc
@@ -60,11 +67,11 @@ MERGEDFILE=$BASE/data/merged.osc
 # make backup of the sequence file
 if cp $BASE/data/sequence_file $BASE/data/sequence_file.old
 then
-   :
+  log_info "make backup copy of sequence_file"
+  :
 else
-   >&2 echo "cannot make backup copy of sequence_file"
-   rm $LOCKFILE
-   exit 1
+  rm $LOCKFILE
+  log_error "cannot make backup copy of sequence_file"
 fi
 
 # todo: describe the purpose of follwing line
@@ -74,41 +81,38 @@ find $BASE/data/ -name replicate-\*.osc -mtime +0 | xargs rm -f
 #  $SEQFILE: the sequence file includes the start sequence number at startup (input value for pyosmium-get-changes )
 #            and the sequence number for next replication procedure (output value of pyosmium-get-changes )
 #  $THISFILE: this file includes the osm data fetched by the script
+log_info "bgn: pyosmium-get-changes sequence=$(cat $SEQFILE)"
 pyosmium-get-changes -s 500 -f $SEQFILE -o $THISFILE
-
-# for test purposes only
-cat $SEQFILE
+log_info "end: pyosmium-get-changessequence=$(cat $SEQFILE)"
 
 # check if the osm diff file excists
 if [ ! -f $THISFILE ]
 then
    # exit the script
-   >&2 echo "pyosmium-get-changes error"
    rm $LOCKFILE
-   exit 1
+   log_error "pyosmium-get-changes error"
 else
-  echo "pyosmium-get-changes passed" >> $LOGFILE
+  log_info "pyosmium-get-changes passed $THISFILE"
 fi
 
 # check if mergefile excists
 if [ -f $MERGEDFILE ]
 then
    # something left over from last time
-   echo "merging with existing diff"
-   echo "merging with existing diff" >> $LOGFILE
+   log_info "merging with existing diff"
    if [ -f $BASE/data/osmium-mc.stderr ]; then
      mv $BASE/data/osmium-mc.stderr $BASE/data/osmium-mc.stderr.old
    fi
    if osmium merge-changes --no-progress -s -o $MERGEDFILE-new.osc $THISFILE $MERGEDFILE 2>$BASE/data/osmium-mc.stderr
    then
+      log_info "osmium passed"
       mv $MERGEDFILE-new.osc $MERGEDFILE
    else
-      >&2 echo "osmium error"
-      echo "osmium error" >> $LOGFILE
       rm $LOCKFILE
-      exit 1
+      log_error "osmium failed"
    fi
 else
+   log_info "skip merging with existing diff"
    cp $THISFILE $MERGEDFILE
 fi
 
@@ -117,31 +121,26 @@ then
   mv $BASE/data/osm2pgsql.stderr $BASE/data/osm2pgsql.stderr.old
 fi
 
-echo "start osm2pgsql"
-echo "start osm2pgsql" >> $LOGFILE
+log_info "start osm2pgsql"
 
 if osm2pgsql -U $PG_USER -H $PG_HOST -d $PG_DBNAME -G -a -s --number-processes=1 -C4000 -S $STYLE --flat-nodes $FLATNODEFILE -e $(($MINZOOM-3))-16 -o $EXPIRELOG --expire-bbox-size 20000 --hstore --tag-transform-script $LUA $MERGEDFILE >$BASE/data/osm2pgsql.stdout 2>$BASE/data/osm2pgsql.stderr
-
 then
-   echo "osm2pgsql passed" >> $LOGFILE
+   log_info "osm2pgsql passed"
    rm $MERGEDFILE
 else
-   >&2 echo "error in osm2pgsql"
-   echo "error in osm2pgsql" >> $LOGFILE
    rm $LOCKFILE
-   exit 1
+   log_error "error in osm2pgsql"
 fi
 
 local_sequence=$(cat $SEQFILE)
 LAG=$(expr $sequenceNumber - $local_sequence)
-echo "end update: osmosis replication is ${LAG} minutes behind upstream"
-echo "end update: osmosis replication is ${LAG} minutes behind upstream" >> $LOGFILE
+log_info "end update: osmosis replication is ${LAG} minutes behind upstream"
 
 if [ -f $BASE/data/doexpire ]; then
   sort -u $EXPIRELOG >$EXPIRELOG.uniq
   mv $EXPIRELOG.uniq $EXPIRELOG
   explog=$(expiremeta.pl --map=mapnikde --minzoom=$MINZOOM <$EXPIRELOG)
-  echo "expire: $explog"
+  log_info "expire: $explog"
   /usr/local/bin/expirehrb
   mv $EXPIRELOG $EXPIRELOG.old
   rm $BASE/data/doexpire
@@ -149,3 +148,4 @@ fi
 
 # remove lockfile
 rm $LOCKFILE
+log_info "end replication $(date)"
