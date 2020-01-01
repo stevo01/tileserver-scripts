208c036d149c5c3aa5c331b4a46b7441b1711fde
Steffen Volkmann <volkmann@devpool.net>
2020-01-01 10:01:00 +0100

    new script creates database indexes

---
+++ src/create_indexes.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+
+LOGFILE=/replication/work/create_indexes.log
+
+export PGPASSWORD=renderer
+PG_USER=renderer
+PG_DBNAME=gis
+PG_HOST=osmpsql
+
+
+function log_info {
+    today=`date +%Y-%m-%d.%H:%M:%S`
+    echo "[INFO ] $today $1" 2>&1 | tee -a $LOGFILE
+}
+
+loginfo "start create indexes"
+cd /replication/src/openstreetmap-carto
+scripts/indexes.py | -U $PG_USER -h $PG_HOST -d $PG_DBNAME 2>&1 | tee -a $LOGFILE
+loginfo "end create indexes"
