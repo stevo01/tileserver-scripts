efbf4c359a11edcad78855e2d8d5418e9d25286b
Steffen Volkmann <volkmann@devpool.net>
2020-01-06 09:54:42 +0100

    determine_latest_osmid: replace ip address by hostname

--- src/determine_latest_osmid.sh
+++ src/determine_latest_osmid.sh
@@ -4,9 +4,7 @@ set -x
 export PGPASSWORD=renderer
 PG_USER=renderer
 PG_DBNAME=gis
-PG_HOST=192.168.1.54
+PG_HOST=osmpsql
 
 psql -d $PG_DBNAME --dbname $PG_DBNAME --host $PG_HOST --username $PG_USER \
      -c "select max(osm_id) from planet_osm_point;"
-
- 
