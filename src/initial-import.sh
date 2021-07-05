b993fdb5a3f2d19c471450b20a6b03542ffd9977
Steffen Volkmann <volkmann@devpool.net>
2021-07-05 07:54:52 +0200

    bugfix in logging function

--- src/initial-import.sh
+++ src/initial-import.sh
@@ -18,9 +18,9 @@ function log_info {
     echo "[INFO ] $today $1" 2>&1 | tee -a $LOGFILE
 }
 
-loginfo "start initial import of database"
+log_info "start initial import of database"
 osm2pgsql -U $PG_USER -H $PG_HOST -d $PG_DBNAME --create -G -s --number-processes 4 -C16000 -S $STYLE --flat-nodes $FLATNODEFILE --hstore --tag-transform-script $LUA $1 2>&1 | tee -a $LOGFILE
-loginfo "end import"
+log_info "end import"
 
 # log database settings
 psql -U $PG_USER -h $PG_HOST -d $PG_DBNAME -c "select name, setting from pg_settings" 2>&1 | tee -a $LOGFILE
